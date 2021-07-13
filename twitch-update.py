01d7cdc01431c2dd55f3438f21fed00beb3fd885
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-07-13 18:27:30 +0300

    updated the .gitignore file

--- twitch-update.py
+++ twitch-update.py
@@ -7,10 +7,20 @@ load_dotenv()
 client_id = os.getenv('Client-ID')
 print(client_id)
 access_token = os.getenv('access_token')
+at_username = os.getenv('at_username')
+at_api_key = os.getenv('at_api_key')
+at.initialize(at_username, at_api_key)
+sms = at.SMS
+app = at.Application
+print(app.fetch_application_data())
+# res = sms.send(message="test", recipients=["+254773428397"])
+# print(res)
+print(sms.fetch_messages(int("ATXid_50c822e96bd467716bf56c543b8c53dc")))
+
 endpoint = "https://api.twitch.tv/helix/streams?"
 headers = {"Client-ID": f"{client_id}",
            "Authorization": f"Bearer {access_token}"}
-params = {"user_login": "Solary"}
+params = {"user_login": ["Solary", "averagejonas"]}
 response = requests.get(endpoint, params=params, headers=headers)
 json_response = response.json()
 print(json_response)
